6957e0a22f7d822c2f444f59d1e2e61fd38a10e4
Anna <noriel266@gmail.com>
2022-06-29 21:41:12 +0300

    refactoring and docstrings

---
+++ README.md
@@ -0,0 +1,5 @@
+## Running example:
+
+    ```
+    python main.py -f google_trans_export.csv -o anki_translations.xlsx
+    ```
--- main.py
+++ main.py
@@ -2,45 +2,69 @@ import argparse
 import pandas as pd
 
 
-def is_sentence(row):
-    text = row.text
-    return text[0].isupper() and text[-1] == '.'
+def is_sentence(text):
+    return text[0].isupper() and text[-1] == '.' and len(text) > 30
 
 
 def preprocess(text):
     return text.strip()
 
 
-def main(input_file, output):
+def filter_languages(df: pd.DataFrame, source_lang: str, target_lang: str) -> pd.DataFrame:
+    """
+    Leaves only the rows that match the given languages. It doesn't matter if the languages are in the wrong order.
+    :param df:
+    :param source_lang: e.g. "Finnish"
+    :param target_lang: e.g. "English"
+    :return:
+    """
+    filtered_df = df[(df.from_lang == source_lang) & (df.to_lang == target_lang)]
+    filtered_reverted_df = df[(df.from_lang == target_lang) & (df.to_lang == source_lang)].rename(
+        columns={"from_lang": "to_lang", "to_lang": "from_lang"}
+    )
+    return filtered_df.append(filtered_reverted_df, ignore_index=True)
 
+
+def filter_sentences(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Removes sentences from the dataframe. Sentences are defined in is_sentence as follows:
+    - First letter is uppercase
+    - Last letter is '.'
+    - Length of the sentence is greater than 30
+    :param df:
+    :return:
+    """
+    df = df[~df.text.apply(is_sentence)]
+    return df[~df.translation.apply(is_sentence)]
+
+
+def preprocess_text(df):
+    df.text = df.text.apply(preprocess)
+    df.translation = df.translation.apply(preprocess)
+    return df
+
+
+def main(input_file, output, source_lang, target_lang):
+    # TODO: if one word, put in the normal form
     df = pd.read_csv(input_file, names=["from_lang", "to_lang", "text", "translation"])
-
-    filtered_df = df[(df.from_lang == "Finnish") & (df.to_lang == "English")]
-    filtered_reverted_df = df[(df.from_lang == "English") & (df.to_lang == "Finnish")]
-    filtered_reverted_new_df = pd.DataFrame({
-        "from_lang": filtered_reverted_df.to_lang,
-        "to_lang": filtered_reverted_df.from_lang,
-        "text": filtered_reverted_df.text,
-        "translation": filtered_reverted_df.translation,
-    })
-    filtered_df = filtered_df.append(filtered_reverted_new_df, ignore_index=True)
-
-    print(filtered_df[filtered_df.text.isnull()])
-
-    print("Output data size", filtered_df.shape[0])
-    filtered_df = filtered_df[~filtered_df.apply(is_sentence, axis=1)]
-    filtered_df.text = filtered_df.text.apply(preprocess)
-    filtered_df.translation = filtered_df.translation.apply(preprocess)
-    print("Output data size without sentences", filtered_df.shape[0])
-
-    filtered_df = filtered_df.rename({"text": "Suomi", "translation": "English"}, axis=1)[["Suomi", "English"]]
-    filtered_df.to_excel(output, index=False, engine="openpyxl")
+    df = filter_languages(df, source_lang, target_lang)
+    print("Data size after filtering languages", df.shape[0])
+    df = filter_sentences(df)
+    print("Data size without sentences", df.shape[0])
+    df = preprocess_text(df)
+    print("Data size after preprocessing", df.shape[0])
+    df = df.rename({"text": "Suomi", "translation": "English"}, axis=1)[["Suomi", "English"]]
+    df.to_excel(output, index=False, engine="openpyxl")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", help="Exported translations from Google Translate in .csv.")
-    parser.add_argument("-o", "--output", help="File to output", default="formatted_translations.xlsx")
-    # TODO put correct file extension if not provided correctly
+    parser.add_argument("-o", "--output", help="File to output", default="anki_translations.xlsx")
+    parser.add_argument("-s", "--source", help="Source language", default="Finnish")
+    parser.add_argument("-t", "--target", help="Target language", default="English")
     args = parser.parse_args()
-    main(args.file, args.output)
+    # TODO check if file exists
+    # TODO check if input file is .csv
+    # TODO for output file, provide a correct extension
+    main(args.file, args.output, args.source, args.target)
